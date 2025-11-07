c70189fb4cee9da73486de79bc1fe7c90ff4bc45
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-11-06 21:32:32 -0500

    update length of samples in json, finish logic for encoding to taking in padded sequences and og lengths

--- utils/encode.py
+++ utils/encode.py
@@ -17,9 +17,10 @@ def encode_sequence(seq, max_len=400):
     ids = []
     for a in seq[:max_len]: #:max_len so it truncates longer sequences so it doesnt ruin memory
         ids.append(aa_to_idx.get(a, 0))  #if an amino acid is not found, assign 0 (PAD)
+    length = min(len(seq), max_len)
     if len(ids) < max_len: #if the sequence is shorter than max length
         ids += [0] * (max_len - len(ids))  #pad to max length for shorter amino acids
-    return torch.tensor(ids) #returns 1D tensor with shape [400] 
+    return torch.tensor(ids), length #returns 1D tensor with shape [400] 
 
 
 #you need to pad because neural nets needs fixed shapes to make mini batches
@@ -32,9 +33,35 @@ os.makedirs("data/encoded", exist_ok=True)
 
 for fam, seqs in data.items():
     tensors = []
-    for s in seqs:
-        tensor = encode_sequence(s, max_len=400)
+    for s in seqs: #for each family of proteins
+        tensor = encode_sequence(s, max_len=400) #convert every string to a fixed length (400) tensor
         tensors.append(tensor)
+    print(f"{fam}: {len(tensors)} sequences")
+    if len(tensors) == 0:
+        print(f"Skipping {fam} - no valid sequences")
+        continue
+    X = torch.stack([e[0] for e in tensors])
+    L = torch.tensor([e[1] for e in tensors]) 
+    torch.save({"X": X, "lengths":L}, f"data/encoded/{fam}.pt") #save one .pt file per family. each file is a list of tensors. 
+    # one file per family to make few-shot smapling easier: pick families, then pick sequences from their respective files 
+    # Now we save X - the padded smaples and L - the og length of each protein
+print("Encoded sequences have been saved to data/encoded/")
+
+
+sample = torch.load("data/encoded/C2H2.pt")
+print(sample.keys())  # dict_keys(['X', 'lengths'])
+print(sample["X"].shape, sample["lengths"].shape)
+
+#test:
+# import glob
+# x = torch.load("data/encoded/C2H2.pt")[0]
+# print(x.shape, x[:30])
+# #torch.size([400] tensor)
+
+# for path in glob.glob("data/encoded/*.pt"):
+#     arr = torch.load(path)
+#     assert all(t.shape[0] == 400 for t in arr), f"Bad length in {path}"
+# print("All sequences are length 400")
 '''
 Each protein is a string of amino-acid letters:
 - but neural networks can't process letters
